35a81f70c09ef14d23de31ba19c4ce208c147893
Ramaraju <buddharajuramaraju@gmail.com>
2018-01-28 21:55:52 +0530

    Assignment1.5

---
+++ Assignment1.5.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# ### Problem Statement
+# 
+# Write a program which accepts a sequence of comma-separated numbers from console
+# and generate a list.
+
+# In[7]:
+
+
+input_sequence = input("Please enter sequence of comma-separated numbers {Ex: 1,2,3,4,5}   ") ## to get comma-separated number
+list = input_sequence.split(",")
+print(list)
+
+
